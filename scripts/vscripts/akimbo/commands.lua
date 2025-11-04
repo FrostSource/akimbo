2e8d78653f1eafa56f05175774c50b041ae5dd96
FrostSource <jjhfrost@gmail.com>
2025-11-04 20:45:13 +1300

    commands overhaul

--- scripts/vscripts/akimbo/commands.lua
+++ scripts/vscripts/akimbo/commands.lua
@@ -1,130 +1,180 @@
 local PlayerData = require "alyxlib.player.data"
 
 ---@return AkimboWeapon
-function CreateAkimboPistol()
-    local name = "akimbo_pistol"
+function CreateAkimboWeapon(baseName, entityClass, scriptPath)
+    local name = baseName
     if Entities:FindByName(nil, name) then
         name = DoUniqueString(name)
     end
-    return SpawnEntityFromTableSynchronous("hlvr_weapon_energygun", {
+    return SpawnEntityFromTableSynchronous(entityClass, {
         targetname = name,
 	    set_spawn_ammo = "10",
-        vscripts = "akimbo/classes/akimbo_pistol"
+        vscripts = scriptPath
     })--[[@as AkimboWeapon]]
 end
 
-function CreateAkimboPistolWithUpgrades(laser, reflex, hopper, burstfire, onUpgraded)
+---@return AkimboPistol
+function CreateAkimboPistol()
+    return CreateAkimboWeapon("akimbo_pistol", "hlvr_weapon_energygun", "akimbo/classes/akimbo_pistol")--[[@as AkimboPistol]]
+end
+
+---@return AkimboRapidfire
+function CreateAkimboRapidfire()
+    return CreateAkimboWeapon("akimbo_smg", "hlvr_weapon_rapidfire", "akimbo/classes/akimbo_rapidfire")--[[@as AkimboRapidfire]]
+end
+
+local AkimboWeaponConfigs = {
+    pistol = {
+        baseName = "akimbo_pistol",
+        entityClass = "hlvr_weapon_energygun",
+        scriptClass = "AkimboPistol",
+        scriptPath = "akimbo/classes/akimbo_pistol",
+        itemField = "energygun",
+        cmdUpgrade = "akimbo_pistol_grant_upgrade",
+        cmdUpgradeBase = "hlvr_energygun_grant_upgrade",
+        upgradeGetter = CBasePlayer.GetPistolUpgrades,
+        upgradeOptions = {
+            pistol_upgrade_laser_sight =   {name="Laser Sight",   id="0"},
+            pistol_upgrade_reflex_sight =  {name="Reflex Sight",  id="1"},
+            pistol_upgrade_bullet_hopper = {name="Bullet Hopper", id="2"},
+            pistol_upgrade_burst_fire =    {name="Burst Fire",    id="3"},
+        }
+    },
+
+    smg = {
+        baseName = "akimbo_smg",
+        entityClass = "hlvr_weapon_rapidfire",
+        scriptClass = "AkimboRapidfire",
+        scriptPath = "akimbo/classes/akimbo_rapidfire",
+        itemField = "rapidfire",
+        cmdUpgrade = "akimbo_smg_grant_upgrade",
+        cmdUpgradeBase = "hlvr_rapidfire_grant_upgrade",
+        upgradeGetter = CBasePlayer.GetRapidfireUpgrades,
+        upgradeOptions = {
+            rapidfire_upgrade_reflex_sight      = {name="Reflex Sight",       id="4"},
+            rapidfire_upgrade_laser_sight       = {name="Laser Sight",        id="5"},
+            rapidfire_upgrade_extended_magazine = {name="Extended Magazine",  id="6"},
+        }
+    },
+}
+
+---@param weaponType "pistol" | "smg" | "shotgun"
+---@param upgrades PlayerWeaponUpgrades[]
+---@param onUpgraded fun(newWeapon: AkimboWeapon)?
+function CreateAkimboWeaponWithUpgrades(weaponType, upgrades, onUpgraded)
+    local cfg = AkimboWeaponConfigs[weaponType]
+    if not cfg then error("Unknown weapon type: " .. tostring(weaponType)) end
+
     local prevEquip = Player.PreviouslyEquipped
-    local currentPistol = Player.Items.weapons.energygun
-    if currentPistol then
+    local currentWeapon = Player.Items.weapons[cfg.itemField]
+    if currentWeapon then
         PlayerData.PauseWeaponStateSync()
-        Player.PrimaryHand:RemoveHandAttachmentByHandle(currentPistol)
+        Player.PrimaryHand:RemoveHandAttachmentByHandle(currentWeapon)
     end
 
-    local newPistol = CreateAkimboPistol()
-    newPistol:DisableAttachmentLogic()
-
-    Player.PrimaryHand:AddHandAttachment(newPistol)
+    local newWeapon = CreateAkimboWeapon(cfg.baseName, cfg.entityClass, cfg.scriptPath)
+    newWeapon:DisableAttachmentLogic()
+    Player.PrimaryHand:AddHandAttachment(newWeapon)
     PlayerData.ResumeWeaponStateSync()
 
-    local upgradeCount =
-        (laser     and 1 or 0) +
-        (reflex    and 1 or 0) +
-        (hopper    and 1 or 0) +
-        (burstfire and 1 or 0)
+    local upgradeCount = 0
+    for _, upgrade in ipairs(upgrades) do
+        if cfg.upgradeOptions[upgrade] then upgradeCount = upgradeCount + 1 end
+    end
 
     local upgradesHeard = 0
-
     local eventListener
-    eventListener = ListenToGameEvent("player_upgraded_weapon", function (params)
-        -- print("Upgrade!", upgradesHeard, "->", upgradesHeard+1, upgradeCount)
+    eventListener = ListenToGameEvent("player_upgraded_weapon", function()
         upgradesHeard = upgradesHeard + 1
-
         if upgradesHeard < upgradeCount then return end
 
         if onUpgraded then
-            print("Upgrade finished")
             StopListeningToGameEvent(eventListener)
-
             PlayerData.PauseWeaponStateSync()
-            Player.PrimaryHand:RemoveHandAttachmentByHandle(newPistol)
-            Player.PrimaryHand:AddHandAttachment(currentPistol)
+            Player.PrimaryHand:RemoveHandAttachmentByHandle(newWeapon)
+            Player.PrimaryHand:AddHandAttachment(currentWeapon)
             PlayerData.ResumeWeaponStateSync()
-
             Player.PreviouslyEquipped = prevEquip
-            Player.Items.weapons.energygun = currentPistol
-            newPistol:EnableAttachmentLogic()
-
-            onUpgraded(newPistol)
+            Player.Items.weapons[cfg.itemField] = currentWeapon
+            newWeapon:EnableAttachmentLogic()
+            onUpgraded(newWeapon)
         end
     end, nil)
 
-    if laser then SendToConsole("hlvr_energygun_grant_upgrade 0") end
-    if reflex then SendToConsole("hlvr_energygun_grant_upgrade 1") end
-    if hopper then SendToConsole("hlvr_energygun_grant_upgrade 2") end
-    if burstfire then SendToConsole("hlvr_energygun_grant_upgrade 3") end
+    for _, upgrade in ipairs(upgrades) do
+        if cfg.upgradeOptions[upgrade] then
+            SendToConsole(cfg.cmdUpgradeBase .. " " .. cfg.upgradeOptions[upgrade].id)
+        end
+    end
 
-    return newPistol
+    return newWeapon
 end
 
-Convars:RegisterCommand("akimbo_pistol_grant_upgrade", function (cmd, upgrade)
-    if upgrade == nil then
-        Msg("usage akimbo_pistol_grant_upgrade X\n0 : Laser Sight\n1 : Reflex Sight\n2 : Bullet Hopper\n3 : Burst Fire\n")
-        return
-    end
+for weaponType, cfg in pairs(AkimboWeaponConfigs) do
+    Convars:RegisterCommand(cfg.cmdUpgrade, function(_, upgrade)
+        if upgrade == nil then
+            Msg("usage " .. cfg.cmdUpgrade .. " X\n")
+            for id, opt in pairs(cfg.upgradeOptions) do
+                Msg(id .. " : " .. opt.name .. "\n")
+            end
+            return
+        end
 
-    if not CurrentAkimboWeapon then
-        Msg("No akimbo pistol equipped, use akimbo_give_pistol first\n")
-        return
-    end
+        local upgradeKey = TableFindKey(cfg.upgradeOptions, function(opt) return opt.id == upgrade end)
+        local upgradeOption = cfg.upgradeOptions[upgradeKey]
+        if not upgradeOption then
+            Warning("Invalid upgrade id\n")
+            Msg("usage " .. cfg.cmdUpgrade .. " X\n")
+            for id, opt in pairs(cfg.upgradeOptions) do
+                Msg(id .. " : " .. opt.name .. "\n")
+            end
+            return
+        end
 
-    local currentAkimboPistol = CurrentAkimboWeapon
-    local currentUpgrades = {}
-    if currentAkimboPistol then
-        currentUpgrades = Player:GetPistolUpgrades(currentAkimboPistol)
+        if not CurrentAkimboWeapon or not isinstance(CurrentAkimboWeapon, cfg.scriptClass) then
+            Msg("No akimbo " .. weaponType .. " equipped, use akimbo_give_" .. weaponType .. " first\n")
+            return
+        end
 
-        if upgrade == "0" and vlua.find(currentUpgrades, "pistol_upgrade_laser_sight") then Msg("Akimbo pistol already has laser sight\n") return end
-        if upgrade == "1" and vlua.find(currentUpgrades, "pistol_upgrade_reflex_sight") then Msg("Akimbo pistol already has reflex sight\n") return end
-        if upgrade == "2" and vlua.find(currentUpgrades, "pistol_upgrade_bullet_hopper") then Msg("Akimbo pistol already has bullet hopper\n") return end
-        if upgrade == "3" and vlua.find(currentUpgrades, "pistol_upgrade_burst_fire") then Msg("Akimbo pistol already has burst fire\n") return end
-    end
+        local currentAkimbo = CurrentAkimboWeapon
+        local currentUpgrades = cfg.upgradeGetter(Player, currentAkimbo)
+        if vlua.find(currentUpgrades, upgradeKey) then
+            Msg("Akimbo " .. weaponType .. " already has " .. upgradeOption.name:lower() .. "\n")
+            return
+        end
 
-    if upgrade == "0" then table.insert(currentUpgrades, "pistol_upgrade_laser_sight")
-    elseif upgrade == "1" then table.insert(currentUpgrades, "pistol_upgrade_reflex_sight")
-    elseif upgrade == "2" then table.insert(currentUpgrades, "pistol_upgrade_bullet_hopper")
-    elseif upgrade == "3" then table.insert(currentUpgrades, "pistol_upgrade_burst_fire")
-    else
-        Msg("usage hlvr_energygun_grant_upgrade X\n0 : Laser Sight\n1 : Reflex Sight\n2 : Bullet Hopper\n3 : Burst Fire\n")
-        return
-    end
+        table.insert(currentUpgrades, upgradeKey)
 
-    CreateAkimboPistolWithUpgrades(
-        vlua.find(currentUpgrades, "pistol_upgrade_laser_sight"),
-        vlua.find(currentUpgrades, "pistol_upgrade_reflex_sight"),
-        vlua.find(currentUpgrades, "pistol_upgrade_bullet_hopper"),
-        vlua.find(currentUpgrades, "pistol_upgrade_burst_fire"),
-
-        function(newPistol)
-            if currentAkimboPistol and currentAkimboPistol:IsEquipped() then
-                currentAkimboPistol:Unequip()
-                currentAkimboPistol:Kill()
+        CreateAkimboWeaponWithUpgrades(weaponType, currentUpgrades, function(newAkimboWeapon)
+            if currentAkimbo and currentAkimbo:IsEquipped() then
+                currentAkimbo:Unequip()
+                currentAkimbo:Kill()
             end
 
-            newPistol:Equip()
+                newAkimboWeapon:Equip()
+        end)
+    end, "Grants an akimbo " .. weaponType .. " upgrade", FCVAR_NONE)
+
+    Convars:RegisterCommand("akimbo_give_" .. weaponType, function()
+        if CurrentAkimboWeapon and isinstance(CurrentAkimboWeapon, cfg.scriptClass) then
+            Msg("Akimbo " .. weaponType .. " already exists\n")
+            return
         end
-    )
-end, "0=laser, 1=reflex, 2=hopper, 3=burstfire", FCVAR_NONE)
 
-Convars:RegisterCommand("akimbo_give_pistol", function (cmd)
-    if CurrentAkimboWeapon then
-        Msg("Akimbo pistol already exists\n")
-        return
-    end
+        local newAkimboWeapon = CreateAkimboWeapon(cfg.baseName, cfg.entityClass, cfg.scriptPath)
 
-    local newPistol = CreateAkimboPistol()
-    Player.SecondaryHand:Drop()
-    newPistol:Equip()
-end, "Gives an akimbo pistol to the player", FCVAR_NONE)
+        if CurrentAkimboWeapon and CurrentAkimboWeapon:IsEquipped() then
+            print("Unequipping other akimbo pistol!", entstr(CurrentAkimboWeapon))
+            PlayerData.PauseWeaponStateSync()
+            CurrentAkimboWeapon:Unequip()
+            CurrentAkimboWeapon:Kill()
+            PlayerData.ResumeWeaponStateSync()
+        end
+
+        devprint("Equipping akimbo " .. weaponType)
+        newAkimboWeapon:Equip()
+    end, "Gives an akimbo " .. weaponType .. " to the player", FCVAR_NONE)
+end
 
 Convars:RegisterCommand("akimbo_return_to_hand", function (cmd)
     local weapon = CurrentAkimboWeapon
